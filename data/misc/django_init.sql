8d3d1000ed31f3c79d2cbdf13b105c300a15ce98
Jay Gee <jayguarino@gmail.com>
2022-09-29 18:41:33 -0700

    added dockerfile and docker compose files

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS school_database;
+CREATE DATABASE school_database;
+
+\c school_database
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
